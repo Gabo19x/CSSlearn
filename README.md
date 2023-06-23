09ebf98c12c7c446fdd83a2f732f41b056391209
Gabo19x <arturopinzon19@gmail.com>
2023-06-23 17:33:30 -0500

    Documentation update 01

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Pagina web sobre aprendizaje de CSS
 
+## Aprende propiedades de CSS para mejorar el UI y UX de tu pagina.
+
 1. **Gradientes:** varios ejemplos de uso de linear-gradient.
 2. **Posiciones:** las diferentes formas de posicionamiento de los objetos.
 3. **Sombra:** la forma de crear y editar una sombra.
--- src/js/animation.js
+++ src/js/animation.js
@@ -5,19 +5,24 @@ const figura3 = document.querySelector('.Figura3');
 const figura4 = document.querySelector('.Figura4');
 const figura5 = document.querySelector('.Figura5');
 
+/* FUNCION Animar
+    Activa o desactiva la animacion segun el objeto correspondiente
+    @params figura: objeto a animar. num: el objeto especifico
+*/
 function Animar(figura, num) {
     if (figura.classList.contains(`Animar_f${num}`)) {
         figura.classList.remove(`Animar_f${num}`);
 
         setTimeout(() => {
             figura.classList.add(`Animar_f${num}`);
-        }, 500);
+        }, 100);
     }
     else {
         figura.classList.add(`Animar_f${num}`);
     }
 }
 
+// Listeners de click para poder activar las animaciones-ñ
 figura1.addEventListener('click', () => {
     Animar(figura1, 1);
 });
